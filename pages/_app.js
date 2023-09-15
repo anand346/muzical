b0be7b656df1b837d89f55a40dd977f92f7fa00a
= <=>
2023-09-15 17:14:49 +0530

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,21 @@ export default function App({ Component, pageProps }) {
           crossorigin="anonymous"></Script>
 
     <Script src="https://www.youtube.com/player_api" ></Script>
+    <Script
+      strategy="lazyOnload"
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+    />
+
+    <Script strategy="lazyOnload">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+      `}
+    </Script>
   </>
   
   )
