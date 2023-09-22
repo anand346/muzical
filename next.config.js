69f79f0a0bf916719ce99dbde5658535295e6132
Anand Raj <64061582+anand346@users.noreply.github.com>
2023-09-22 06:57:25 +0000

    modified next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images:{
-    domains: ['source.unsplash.com','localhost','muzical.vercel.app']
+    domains: ['source.unsplash.com','localhost','muzical.vercel.app','127.0.0.1']
   }
 }
 
