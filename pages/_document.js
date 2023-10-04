84cd3e170e3d72d47c195e44cc95a46e79808f97
shubham <singhshubham3006.ss@gmail.com>
2023-10-04 13:09:08 +0530

    Add title on the landing page Muzical

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,12 @@
-import { Html, Head, Main, NextScript , Script } from 'next/document'
+import { Html, Head, Main, NextScript, Script } from 'next/document'
 
 export default function Document() {
   return (
 
-    
+
     <Html lang="en">
       <Head>
-        
+        <title>Muzical</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
@@ -17,7 +17,7 @@ export default function Document() {
         <Main />
         <NextScript />
 
-        
+
       </body>
     </Html>
   )
